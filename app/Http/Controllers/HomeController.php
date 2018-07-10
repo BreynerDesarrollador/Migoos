79020e744da1703d93995ccabc69ade2fafc02c2
Breyner Perez Bolaño <38225163+BreynerDesarrollador@users.noreply.github.com>
2018-07-10 14:30:36 -0500

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -92,7 +92,7 @@ class HomeController extends Controller
     
         public function pruebacola(){
         try{
-            $user=User::all()->take(1);
+            $user=User::find(4);
             dispatch(new enviarverificacionemail($user));
             return view('emails.verificacion')->with(["correo"=>$user->email]);
         }catch (Excepcion $es){

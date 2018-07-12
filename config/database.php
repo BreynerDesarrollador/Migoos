56f2d5c891396db618e1e81d886c483afdc3b6e2
Breyner Perez Bolaño <38225163+BreynerDesarrollador@users.noreply.github.com>
2018-07-12 16:26:28 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -40,11 +40,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', ''),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'migoos'),
+            'username' => env('DB_USERNAME', 'breynerde'),
+            'password' => env('DB_PASSWORD', 'breyner1995'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

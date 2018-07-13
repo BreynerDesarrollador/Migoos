92e6d0cf4914fa72e23f5290b378307c678560b2
BreynerDesarrollador <38225163+BreynerDesarrollador@users.noreply.github.com>
2018-07-13 13:39:38 -0500

    isAdmin
    Co-Authored-By: Breyner Perez Bolaño <breynerdesarrollador@users.noreply.github.com>

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -15,7 +15,7 @@ class isAdmin
      */
     public function handle($request, Closure $next)
     {
-        if(auth()->user()->isAdmin()) {
+        if(auth()->user()->type==0) {
             return $next($request);
         }
         return redirect('/');

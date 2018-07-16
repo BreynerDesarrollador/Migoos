b88a3be356b71871bb02c2e557e60a3d67a7d9f2
BreynerDesarrollador <38225163+BreynerDesarrollador@users.noreply.github.com>
2018-07-16 17:19:00 -0500

    fdfdsfd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -123,7 +123,7 @@ class HomeController extends Controller
             if (!session('event_ubicacion')) {
                 $user_ip = $_SERVER['REMOTE_ADDR'];
                 //http://api.ipstack.com/$user_ip?access_key=37ada1cf3258ff604438208b0c5091bd&output=json
-                $ch = curl_init("http://ipapi.co/json");
+                $ch = curl_init("https://ipapi.co/json");
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                 $country_code = curl_exec($ch);
                 session(['event_ubicacion' => $country_code]);// = $country_code;
